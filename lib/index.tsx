c61016d1b556177ae0eca6636846244997583b58
lingxiangyang <1150689758@qq.com>
2019-04-14 17:33:25 +0800

    chore: 区分开发环境和生产环境

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,1 +1,7 @@
-console.log('hello');
+// @ts-ignore
+import React from 'react';
+// @ts-ignore
+import ReactDOM from 'react-dom';
+
+console.log(React);
+console.log(ReactDOM);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: {
-        // MOUSUI 为输出的文件名
-        MOUSUI: './lib/index.tsx'
+        // index 为输出的文件名
+        index: './lib/index.tsx'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
     },
     output: {
         path: path.resolve(__dirname, 'dist/lib'),
@@ -18,11 +20,5 @@ module.exports = {
                 loader: 'awesome-typescript-loader'
             }
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Mous-UI',
-            template: 'index.html'
-        })
-    ]
+    }
 }
