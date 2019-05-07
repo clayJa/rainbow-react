407f32e151caaf097ddbedd368def69b077de3e4
lingxiangyang <1150689758@qq.com>
2019-05-07 22:01:55 +0800

    feat: 初步完成icon组件

---
+++ lib/icon/icon.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../icons/wechat.svg';
+interface IconProps {
+    name: string;
+}
+const Icon: React.FunctionComponent<IconProps> = (props) => {
+    return (
+        <span>
+            <svg><use xlinkHref={`#${props.name}`}/></svg>
+        </span>
+    );
+}
+
+export default Icon;
