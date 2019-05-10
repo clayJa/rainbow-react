5e15be8237c87a9424769d97bf900d49417f1f39
lingxiangyang <1150689758@qq.com>
2019-05-10 23:35:06 +0800

    chore: npm public package.json设置

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,10 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Icon from './icon/icon';
 
-ReactDOM.render(
-    <div>
-        <Icon name="wechat" onClick={()=>{console.log('hhh')}} onMouseEnter={() =>{console.log('111')}} className={undefined}/>
-        <Icon name="github"/>
-        <Icon name="qq"/>
-    </div>, document.querySelector('#root'));
+export {default as Icon} from './icon/icon';
