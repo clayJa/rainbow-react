d154bb6ae55a4f9891b465ae023a07c39e354f30
lingxiangyang <1150689758@qq.com>
2019-05-12 22:35:44 +0800

    feat: 增加example

---
+++ lib/icon/icon.example.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Icon from './icon';
+
+
+const IconExample: React.FunctionComponent = () => {
+  return (
+    <div>
+      <Icon name="github"/>
+      <Icon name="wechat"/>
+      <Icon name="qq"/>
+    </div>
+  );
+};
+
+export default IconExample;
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,10 +2,13 @@ const base = require('./webpack.config')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = Object.assign({}, base, {
     mode: 'development',
+    entry: {
+        example: './example/index.tsx',
+    },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Mous-UI',
-            template: 'index.html'
+            template: './example/index.html'
         })
     ],
 })
