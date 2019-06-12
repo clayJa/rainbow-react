440c358d0b79d346ceb09e29c71f89b32e8ae74a
lingxiangyang <1150689758@qq.com>
2019-06-13 00:23:26 +0800

    test: 测试 dialog 与 helper 函数

---
+++ lib/dialog/__tests__/dialog.unit.jsx
@@ -0,0 +1,84 @@
+import * as renderer from 'react-test-renderer'
+import * as React from 'react'
+import Dialog, {confirm, alert, modal} from '../dialog'
+import {mount} from 'enzyme'
+
+describe('dialog', () => {
+  const clearDom = () => {
+    document.body.innerHTML = ''
+  }
+  it('render successfully', () => {
+    const onClose = jest.fn()
+    const props = {
+      visible: true,
+      onClose,
+    }
+    const component = mount(<Dialog {...props}>这是一个对话框</Dialog>)
+    expect(component.find('.mous-dialog')).toHaveLength(1)
+    expect(component.find('.mous-dialog-mask')).toHaveLength(1)
+    clearDom()
+  })
+  it('onClose', () => {
+    const onClose = jest.fn()
+    const props = {
+      visible: true,
+      onClose,
+    }
+    const component = mount(<Dialog {...props}>这是一个对话框</Dialog>)
+    component.find('.mous-dialog-close').simulate('click')
+    expect(onClose).toBeCalled()
+    clearDom()
+  })
+  it('closeOnClickMask', () => {
+    const onClose = jest.fn()
+    const props = {
+      visible: true,
+      onClose,
+      closeOnClickMask: true,
+    }
+    const component = mount(<Dialog {...props}>这是一个对话框</Dialog>)
+    component.find('.mous-dialog-mask').simulate('click')
+    expect(onClose).toBeCalled()
+    clearDom()
+  })
+  it('closeOnClickMask', () => {
+    const onClose = jest.fn()
+    const props = {
+      visible: true,
+      onClose,
+      buttons: [<button>确定</button>],
+    }
+    const component = mount(<Dialog {...props}>这是一个对话框</Dialog>)
+    expect(component.find('button')).toHaveLength(1)
+    clearDom()
+  })
+  it('modal', () => {
+    const afterClose = jest.fn()
+    modal(<h1>你好</h1>,[<button>按钮</button>],afterClose)
+    expect(document.body.querySelectorAll('h1')).toHaveLength(1)
+    document.body.querySelectorAll('.mous-dialog-close')[0].click()
+    clearDom()
+  })
+  it('confirm ok', () => {
+    const ok = jest.fn()
+    const cancel = jest.fn()
+    confirm('1', ok, cancel,'ok')
+    document.body.querySelectorAll('button.mous-button-primary')[0].click()
+    expect(ok).toBeCalled()
+    clearDom()
+  })
+  it('confirm cancel', () => {
+    const ok = jest.fn()
+    const cancel = jest.fn()
+    confirm('1', ok, cancel)
+    document.body.querySelectorAll('button.mous-button-normal')[0].click()
+    expect(cancel).toBeCalled()
+    clearDom()
+  })
+  it('alert', () => {
+    alert('1')
+    const html = document.body.querySelectorAll('.mous-dialog-main')[0].innerHTML
+    expect(html).toEqual('1')
+    clearDom()
+  })
+})
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,2 +1,5 @@
 
 export {default as Icon} from './icon/icon';
+export {default as Button} from './button/button';
+export {default as Dialog} from './dialog/dialog';
+export {confirm, alert, modal} from './dialog/dialog';
