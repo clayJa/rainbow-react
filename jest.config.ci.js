05e2d2832486c06415216a81571616cb5deff3f3
lingxiangyang <1150689758@qq.com>
2019-06-16 20:28:59 +0800

    删除多余文件

--- jest.config.ci.js
+++
@@ -1,8 +0,0 @@
-const base = require('./jest.config')
-module.exports = Object.assign({}, base, {
-    reporters: ["jest-junit"],
-    collectCoverage: true,
-    collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**","!lib/**/*.example.{ts,tsx}","!lib/**/*.demo.{ts,tsx}"],
-    coverageDirectory: 'coverage',
-    coverageReporters: ['text', 'lcov'],
-})
--- lib/index.tsx
+++
@@ -1,5 +0,0 @@
-
-export {default as Icon} from './icon/icon';
-export {default as Button} from './button/button';
-export {default as Dialog} from './dialog/dialog';
-export {confirm, alert, modal} from './dialog/dialog';
